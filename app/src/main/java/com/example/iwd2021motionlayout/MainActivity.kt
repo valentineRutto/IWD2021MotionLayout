87668feca56501be9ec36c41d2d02048765e124d
ValentinRutto <valentine.rutto@dlight.com>
2021-03-12 22:43:08 +0300

    Add support action bar

--- app/src/main/java/com/example/iwd2021motionlayout/MainActivity.kt
+++ app/src/main/java/com/example/iwd2021motionlayout/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setIcon(R.drawable.android)
+        supportActionBar?.title = "Motion Layout Demo"
     }
 }
